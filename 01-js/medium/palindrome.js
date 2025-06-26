9251331bbc1930d6897275c76169d9abd5efa273
ADITYATIWARI342005 <142050150+ADITYATIWARI342005@users.noreply.github.com>
2025-06-26 11:23:01 +0530

    palindrome.js completed and all tests passed

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -3,8 +3,27 @@
   Note: the input string is case-insensitive which means 'Nan' is a palindrom as 'N' and 'n' are considered case-insensitive.
 */
 
+function isAlpha(char) {
+  return /^[A-Za-z]$/.test(char);
+}
+
 function isPalindrome(str) {
+  let test="";
+
+  for(let i = 0; i < str.length; i++){
+    if(isAlpha(str[i])){
+      test+=str[i].toLowerCase();
+    }
+  }
+
+  for(let i=1; i<=test.length; i++){
+    if(test[i]!=test[test.length-1-i]){
+      return false;
+    }
+  }
+
   return true;
+
 }
 
 module.exports = isPalindrome;
