b242d1f29aa5140509cb1713ae894e571823cbe0
ADITYATIWARI342005 <142050150+ADITYATIWARI342005@users.noreply.github.com>
2025-06-26 11:14:53 +0530

    countVowels.js completed and all tests passed

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -5,8 +5,21 @@
   Once you've implemented the logic, test your code by running
 */
 
+function isVowel(v) {
+    if(v=='a' || v=='e' || v=='i' || v=='o' || v=='u') return true;
+    return false;
+}
+
 function countVowels(str) {
     // Your code here
+    let res=0;
+    let test=str.toLowerCase();
+    for(let i = 0; i < test.length; i++){
+        if(isVowel(test[i])) {
+            res++;
+        }
+    }
+    return res;
 }
 
 module.exports = countVowels;
